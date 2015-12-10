2caf63516f7cfea78455dacc8eaa3d0c829dfc03
Winston Durand <me@winstondurand.com>
2015-12-09 19:09:58 -0800

    added coefficient support

--- lib/equation_system/system.rb
+++ lib/equation_system/system.rb
@@ -1,20 +1,25 @@
 module EquationSystem
-  class System 
+  class System
     def initialize(variables, range: -10..10, allow_zero: false, failsafe: 10)
       raise FailSafeError, "Failsafe cannot be less than 1" if failsafe < 1
       @prng = Random.new
+      @coefficients = []
       @answers = generate_answers(variables, range, allow_zero)
       @equations = generate_equations(@answers, range, allow_zero, failsafe)
     end
-  
+
     def equations
       @equations
     end
-  
+
     def answers
       @answers
     end
-  
+
+    def coefficients
+      @coefficients
+    end
+
     def method_missing(method, *args, &block)
       match = method.to_s.match(/^solution_for_([a-z]+)$/)
       if match && @answers[match[1].to_sym]
@@ -23,24 +28,26 @@ module EquationSystem
         super
       end
     end
-    
+
     def to_json
       require 'json' unless defined?(JSON)
       {:equations => @equations, :variables => @answers}.to_json
     end
-  
+
     private
-  
+
     def generate_equations(answers, range, allow_zero, failsafe)
       equations = []
       answers.length.times do
         failsafe.times do |fails|
+          temp = []
           equation = ""
           total = 0
           answers.each do |var, val|
-            coeffecient = random_num(range, allow_zero)
-            total += coeffecient * val
-            equation << " %+#{val.is_a?(Float) ? 'f' : 'd' }%s" % [coeffecient, var]
+            coefficient = random_num(range, allow_zero)
+            temp << coefficient
+            total += coefficient * val
+            equation << " %+#{val.is_a?(Float) ? 'f' : 'd' }%s" % [coefficient, var]
           end
           equation = equation.gsub(/(\+|-)/, '\1 ').gsub(/1([a-z])/, '\1').gsub(/^ (-)?\+? /, '\1')
           equation << " = #{total}"
@@ -48,18 +55,19 @@ module EquationSystem
             raise RangeTooSmallError, "Unable to get unique equation after #{failsafe} tries" unless fails == failsafe - 1
           else
             equations << equation
+            @coefficients << {variables: temp, sum: total}
             break
           end
-        end 
+        end
       end
       equations
     end
-  
+
     def generate_answers(variables, range, allow_zero)
       ret = {}
       vars = []
-      if variables <= 3 && variables > 0
-        vars = %w(x y z)[0, variables]
+      if variables <= 4 && variables > 0
+        vars = %w(x y z w)[0, variables].sort
       elsif variables > 3
         cur = 'a'
         variables.times do
@@ -73,10 +81,10 @@ module EquationSystem
       vars.each do |var|
         ret[var.to_sym] = random_num(range, allow_zero)
       end
-    
+
       ret
     end
-  
+
     def random_num(range, allow_zero)
       r = 0
       begin
